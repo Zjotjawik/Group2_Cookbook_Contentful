4f21285ba1b263fd05311b96ff91ac74ddba5f71
Christoph Suessmeier <mail@suessmeier.net>
2023-08-11 11:12:54 +0200

    Removed react-helmet to get rid of an 'unsafe' message

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import HeroRecipe from "../components/HeroRecipe";
 import RecipeList from "../components/RecipeList";
 import useContentful from "../hooks/useContentful";
-import { Helmet } from "react-helmet";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
 
@@ -47,9 +46,6 @@ const Homepage = () => {
   return (
     <>
       {/* Update title of page */}
-      <Helmet>
-        <title>Welcome to WBS Group 2 Cookbook</title>
-      </Helmet>
       <div className="my-10 grid gap-y-10 max-w-7xl mx-auto">
         {/* Loading */}
         {isLoading && <Loading />}
